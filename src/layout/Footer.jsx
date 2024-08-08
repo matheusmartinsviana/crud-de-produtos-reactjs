3db00ce9531d612426ae063faa0167e0be5dded1
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-08-08 20:41:20 -0300

    style: navbar proportion sizes

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Styles/Footer.module.css'
 const Footer = (() => {
     return (
     <footer className={styles.footerContainer}>
-        
+        S
     </footer>
     );
 })
