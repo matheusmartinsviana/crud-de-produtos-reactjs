ee69aa35300edad7d0804897fd65131bd259fa1c
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-08-08 20:25:21 -0300

    feat: adding useEffect to login if no have a token

--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -1,14 +1,25 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import ProductsView from "../components/ProductsView";
+import { useEffect } from "react";
 
 const Body = (() => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            navigate("/")
+        } else {
+            navigate("/login")
+        }
+    })
+
     return (
         <>
-            <div style={{ display: "flex", width: "100%", minHeight: "100%"}}>
+            <div style={{ display: "flex", width: "100%", minHeight: "100%" }}>
                 <Navbar />
-                <div style={{ padding: "40px", height: "100vh"}}>
+                <div style={{ padding: "40px", height: "100vh" }}>
                     <ProductsView />
                     <Outlet />
                 </div>
