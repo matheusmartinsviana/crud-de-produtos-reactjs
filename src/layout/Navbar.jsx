0b76507af44fa9478135b3a9af7fa665aa01cf17
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-08-08 21:31:52 -0300

    style: navbar hover effect and title

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,11 +4,18 @@ import styles from './Styles/Navbar.module.css'
 const Navbar = (() => {
     return (
         <nav className={styles.navbarContainer}>
+            <h1>CRUD de Produtos</h1>
             <ul>
                 <li>
                     <Link to="/" >Ver produtos</Link>
+                </li>
+                <li>
                     <Link to="/add" >Adicionar Produto</Link>
+                </li>
+                <li>
                     <Link to="/update" >Atualizar Produto</Link>
+                </li>
+                <li>
                     <Link to="/delete" >Deletar Produto</Link>
                 </li>
             </ul>
